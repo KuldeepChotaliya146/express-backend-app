9bfd35e6af0c59f89d3b6d2cccea4211abd0ec65
Kuldeep Chotaliya <kuldeepchotaliya17@gmail.com>
2023-12-23 18:33:16 +0530

    Added default middlewares to support json, query params, static files and cookie setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import express from "express"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express()
 
+// To handle CORS related issues
+app.use(cors({
+  origin: process.env.ORIGIN,
+  credentials: true
+}))
 
+app.use(express.json({ limit: '20kb'})) // To define size of json which can expect
+app.use(express.urlencoded({ extended: true, limit: "16kb" })) // To deal with query parameters
+app.use(express.static("public")) // To handle static assets
+app.use(cookieParser()) // To set and get cookies from client 
 
 export default app
